931f8b211c59a1aaf4069d936f3322e296d63d70
Felix Khodakovsky <felix.khodakovsky@quantumsoft.ru>
2021-09-23 15:04:19 +1000

    add client code

--- comments/index.js
+++ comments/index.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
+const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 const commentsByPostId = {};
 
 app.get('/posts/:id/comments', (req, res) => {
-  res.send(commentsByPostId[req.params.id]);
+  res.send(commentsByPostId[req.params.id] || []);
 });
 
 app.post('/posts/:id/comments', (req, res) => {
@@ -18,6 +20,7 @@ app.post('/posts/:id/comments', (req, res) => {
   const comments = commentsByPostId[req.params.id] || [];
 
   comments.push({ id: commentId, content });
+
   commentsByPostId[req.params.id] = comments;
 
   res.status(201).send(comments);
